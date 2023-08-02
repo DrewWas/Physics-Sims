d7e47399142f2f26fe56965823d441e40a6b9c95
Andrew Wasielewski <awwasielewski@gmail.com>
2023-08-01 20:03:35 -0400

    lemme finish this one part before I go

--- single_pendulum.py
+++ single_pendulum.py
@@ -5,25 +5,46 @@ pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("single pendulum")
 
+
 # constants
 FPS = 60
+ROPELEN = 200
 black = (0,0,0)
 blue = (0, 138, 255)
 white = (255,255,255)
 red = (255,23,54)
 gray = (130,130,130)
-
+middle_block_pos = (294, 133)
 
 
 def getpos():
+    from math import atan, sin, cos, degrees
     pos = pygame.mouse.get_pos()
-    return (pos)
+
+    # get the angle from the square the 'pos' is and then draw the
+    # circle at that same angle from the square ROPELEN away 
+
+
+    # avoid division by zero error (kinda shitty)
+    if pos[1] == middle_block_pos[1]:
+        y_diff = 0.00001 
+    else: 
+        y_diff = (pos[1] - middle_block_pos[1])
+
+    x_diff = (pos[0] - middle_block_pos[0])
+    tanTheta = (x_diff/y_diff)
+    theta_rad = atan(tanTheta)
+
+    print("degrees: " + str(degrees(theta_rad)))
+
+    coord = None 
+
+
+    return coord
 
 
 
 def draw(ball_pos):
-    middle_block_pos = (294, 133)
-
     WIN.fill(black)
     pygame.draw.rect(WIN, red, (290, 130, 10, 10))
 
@@ -35,13 +56,9 @@ def draw(ball_pos):
 
 
 
-    # Upon click, the ball pos (and therefore string) are temporarily
-    # locked until the sim starts (spacebar is hit... or any key idk
-    # yet)
 
-
-    # ball can only move along the path of the string (can it move
-    # inwards????)
+    # ball can only move along the path of the string (this sim assumes
+    # a tought line)
 
     pygame.display.update()
 
@@ -60,6 +77,7 @@ def main():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 final_pos = not final_pos
+
 
 
         if final_pos == False:
