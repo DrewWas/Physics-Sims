8b261db7b586b17bcbc1bfef0b025185a2002160
Andrew Wasielewski <awwasielewski@gmail.com>
2023-08-10 23:41:21 -0400

    this is a lot less code so far

--- dingle_pendulum.py
+++ dingle_pendulum.py
@@ -9,7 +9,7 @@ white = (255,255,255)
 gray = (128,128,128)
 blue = (0,138,255)
 red = (255, 23, 54)
-
+gameStart = False
 
 run = True
 while run:
@@ -17,16 +17,31 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            gameStart = not gameStart
+            ball_pos = init_pos
 
     win.fill(black)
+
+    #Draw a single block to the screen (anchor point)
     pygame.draw.rect(win, red, ((WIDTH // 2) - 20, (HEIGHT // 2) - 125, 10, 10))
+
+
+    if not gameStart:
+        init_pos = pygame.mouse.get_pos()
+        pygame.draw.circle(win, blue, (init_pos), 20)
+
+    if gameStart:
+        pygame.draw.circle(win, blue, (ball_pos), 20)
+
+
     pygame.display.update()
 
 
 """
 PLAN:
 
-* Draw a single block to the screen (anchor point)
+* Draw a single block to the screen (anchor point) *************
 
 * Draw a ball to the screen with variable x and y coords
 
